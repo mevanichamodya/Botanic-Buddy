32b6dea3cbc644f0eb165845dc49c65e557fe385
mevanichamodya <mevani2001@gmail.com>
2023-11-17 15:04:13 +0530

    issue fixed in login layout

--- src/screens/loginScreen.tsx
+++ src/screens/loginScreen.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const loginScreen = () => {
   return (
     <View style={[styles.main]}>
+      <View style={[styles.imagBox]} />
       <View style={[styles.loginBox, {flexDirection: 'column'}]}>
         <View style={[styles.loginHeader]}>
-          <View style={[styles.loginHear]} />
-          <View style={[styles.loginText]} />
+          <View style={[styles.loginHear]}>
+            <Text style={[styles.loginMainText]}>Login here</Text>
+          </View>
+          <View style={[styles.loginText]}>
+            <Text style={[styles.loginSubText]}>Welcome back you've</Text>
+            <Text style={[styles.loginSubText]}>been missed!</Text>
+          </View>
         </View>
 
         <View style={[styles.loginInput]}>
-          <View style={[styles.emailInput]} />
-          <View style={[styles.passwordInput]} />
-          <View style={[styles.signinInput]} />
+          <View style={[styles.emailInput]}>
+            <Text style={[styles.inputText]}>Email</Text>
+          </View>
+          <View style={[styles.passwordInput]}>
+            <Text style={[styles.inputText]}>Password</Text>
+          </View>
+          <View style={[styles.signinInput]}>
+            <Text style={[styles.signInText]}>Sign In</Text>
+          </View>
         </View>
         <View style={[styles.loginButton]}>
-          <View style={[styles.newAccText]} />
-          <View style={[styles.continueText]} />
-          <View style={[styles.googleButton]} />
-          <View style={[styles.fbButton]} />
+          <View style={[styles.newAccTextField]}>
+            <Text style={[styles.newAccText]}> Create new account</Text>
+          </View>
+          <View style={[styles.continueTextField]}>
+            <Text style={[styles.continueText]}> Or continue with</Text>
+          </View>
+          <View style={[styles.buttonField]}>
+            <View style={[styles.googleButton]} />
+            <View style={[styles.fbButton]} />
+          </View>
         </View>
       </View>
     </View>
@@ -29,118 +47,194 @@ const loginScreen = () => {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    padding: 30,
+    padding: 0,
     backgroundColor: 'green',
     flexDirection: 'column',
   },
 
+  imagBox: {
+    width: '100%',
+    height: 400,
+    backgroundColor: 'purple',
+    position: 'absolute',
+  },
+
   loginBox: {
     flex: 1,
-    //padding: 10,
+    padding: 10,
     marginTop: 80,
     backgroundColor: 'white',
     borderRadius: 18,
     elevation: 50,
+    margin: 30,
   },
 
   loginHeader: {
-    flex: 3,
-    backgroundColor: 'black',
-    margin: 10,
+    flex: 2,
+    backgroundColor: 'white',
+    margin: 5,
   },
 
   loginHear: {
-    flex: 0.8,
-    marginTop: 30,
-    backgroundColor: 'yellow',
-    margin: 20,
+    flex: 1,
+    marginTop: 10,
+    backgroundColor: 'white',
+    margin: 0,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  loginMainText: {
+    fontSize: 28,
+    fontWeight: '900',
+    textShadowColor: '#009A17',
+    color: '#009A17',
+    letterSpacing: 0.6,
   },
 
   loginText: {
-    flex: 0.6,
-    marginTop: 2,
-    backgroundColor: 'orange',
-    margin: 5,
+    //flex: 0.6,
+    marginTop: 0,
+    backgroundColor: 'white',
+    margin: 0,
     alignItems: 'center',
     justifyContent: 'center',
   },
 
+  loginSubText: {
+    fontSize: 16,
+    color: '#0C0B0B',
+    textAlign: 'center',
+    fontWeight: '500',
+    letterSpacing: 1,
+  },
+
   loginInput: {
-    flex: 4.3,
-    backgroundColor: 'pink',
-    margin: 2,
+    flex: 2.7,
+    backgroundColor: 'white',
   },
 
   emailInput: {
     flex: 0.4,
-    marginTop: 20,
-    backgroundColor: 'black',
-    margin: 8,
-    alignItems: 'center',
+    marginTop: 10,
+    backgroundColor: 'white',
+    margin: 3,
     justifyContent: 'center',
     borderRadius: 15,
-    elevation: 30,
+    elevation: 10,
+    borderWidth: 3,
+    borderColor: '#009A17',
   },
   passwordInput: {
     flex: 0.4,
     marginTop: 20,
-    backgroundColor: 'black',
-    margin: 8,
-    alignItems: 'center',
+    backgroundColor: 'white',
+    margin: 3,
     justifyContent: 'center',
     borderRadius: 15,
-    elevation: 30,
+    elevation: 10,
+    borderWidth: 3,
+    borderColor: '#009A17',
   },
   signinInput: {
     flex: 0.4,
     marginTop: 20,
-    backgroundColor: 'black',
-    margin: 8,
-    alignItems: 'center',
+    backgroundColor: '#009A17',
+    margin: 3,
     justifyContent: 'center',
     borderRadius: 15,
-    elevation: 30,
+    elevation: 10,
+    borderWidth: 3,
+    borderColor: '#009A17',
+  },
+
+  inputText: {
+    fontSize: 15,
+    color: '#5B5959',
+    fontWeight: '500',
+    letterSpacing: 0.5,
+    textAlign: 'left',
+    marginLeft: 20,
+  },
+
+  signInText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: '900',
+    letterSpacing: 1,
+    textAlign: 'center',
   },
 
   loginButton: {
     flex: 3,
-    backgroundColor: 'blue',
-    margin: 10,
+    backgroundColor: 'white',
+  },
+
+  newAccTextField: {
+    flex: 0.3,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   newAccText: {
+    fontSize: 16,
+    color: '#000000',
+    fontWeight: '500',
+    letterSpacing: 0.8,
+  },
+
+  continueTextField: {
     flex: 0.3,
-    backgroundColor: 'gray',
+    backgroundColor: 'white',
+    marginTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   continueText: {
-    flex: 0.3,
-    backgroundColor: 'red',
-    marginTop: 40,
+    fontSize: 16,
+    color: '#FF0505',
+    fontWeight: 'bold',
+    letterSpacing: 0.8,
+  },
+
+  buttonField: {
+    width: '100%',
+    height: 60,
+    backgroundColor: 'white',
+    marginTop: 10,
+    display: 'flex',
+    margin: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   googleButton: {
-    width: 65,
+    width: 70,
     height: 50,
-    backgroundColor: 'blue',
-    borderRadius: 7,
-    elevation: 30,
+    backgroundColor: 'white',
+    borderRadius: 9,
+    elevation: 10,
     alignItems: 'center',
     justifyContent: 'center',
-    marginRight: 3,
+    marginRight: 10,
+    borderWidth: 2,
+    borderColor: '#009A17',
   },
 
   fbButton: {
-    width: 65,
+    width: 70,
     height: 50,
     backgroundColor: 'white',
-    borderRadius: 7,
-    elevation: 30,
+    borderRadius: 9,
+    elevation: 10,
     alignItems: 'center',
     justifyContent: 'center',
-    marginLeft: 3,
+    marginLeft: 10,
+    borderWidth: 2,
+    borderColor: '#009A17',
   },
 });
 
