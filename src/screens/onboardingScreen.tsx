99a56cb266a2138bf9153148c4f7413e05638c8a
Hansama Dewindi Rajaguru <141362143+HansamaRajaguru@users.noreply.github.com>
2023-11-20 21:51:05 -0800

    initialized

---
+++ src/screens/onboardingScreen.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+const Onboard=()=>{
+  return(
+    <SafeAreaView style={styles.container}>
+    
+    </SafeAreaView>
+  );
+  
+};
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    borderColor:'#009A17',
+    borderWidth:6,
+    borderRadius:20,
+    //overflow:'hidden'
+  }
+});
+export default Onboard;
