58a0f1d66344d6634f5ecd7fe5b8b7deaa63f4cf
Madhusha Chinthani <madhuomchinthani@gmail.com>
2023-11-23 20:43:40 +0530

    Create frontend of screen.
    I created frontend of screen.

--- App.tsx
+++ App.tsx
@@ -1,23 +1,44 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ImageBackground, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+const PlantDiseaseScreen: React.FC = () => {
+  const [searchText, setSearchText] = useState<string>('');
 
-import React from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+  const handleSearch = () => {
+    // Add your logic for handling the search here
+    alert('Searching for: ' + searchText);
+  };
 
-const PlantDiseaseScreen = () => {
   return (
     <ImageBackground
-      source={{ uri: "https://cdn.mos.cms.futurecdn.net/7sg7FXztWiRzCYGLmpbCmC-1200-80.jpg" }} 
+      source={{ uri: 'https://eskipaper.com/images/flowers-sunlight-nature-1.jpg' }}
       style={styles.backgroundImage}
     >
       <View style={styles.container}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.titleText}>Diseases...</Text>
+        <View style={styles.cloudContainer}>
+          <Text style={styles.titleText}>Plant Diseases🍁 </Text>
         </View>
-      
-            <Text style={styles.paragraphText}></Text>
-    
+
+        <Text style={styles.paragraphText}>
+          Plant diseases can affect various parts of plants including leaves, stems, roots, and fruits. They are often caused by
+          pathogens such as fungi, bacteria, viruses, nematodes, and other microorganisms.🍂🥀
+          Explore and learn about common plant diseases. Select a disease to view details.
+        </Text>
+
+        {/* Small Search Bar */}
+        <View style={styles.searchContainer}>
+          <Icon name="search" size={15} color="black" style={styles.searchIcon} />
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Search"
+            placeholderTextColor="gray"
+            value={searchText}
+            onChangeText={(text) => setSearchText(text)}
+            onSubmitEditing={handleSearch}
+          />
+        </View>
       </View>
-    
     </ImageBackground>
   );
 };
@@ -25,36 +46,60 @@ const PlantDiseaseScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'transparent', 
+    backgroundColor: 'transparent',
     justifyContent: 'center',
     alignItems: 'center',
-   
   },
   backgroundImage: {
     flex: 1,
     resizeMode: 'cover',
+    opacity: 0.85,
   },
-  titleContainer: {
-    backgroundColor: '#32CD32',
-    padding: 20,
-    alignItems: 'center',
-    borderRadius: 100,
-    width: 200,
-    marginBottom :500,
+  cloudContainer: {
+    backgroundColor: 'white',
+    padding: 15,
+    borderRadius: 50,
+    marginBottom: 20,
+    opacity: 1,
+    position: 'absolute',
+    top: 40,
   },
   titleText: {
     color: 'black',
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
-   
   },
-
-  paragraphText :{
-    color: 'white',
+  paragraphText: {
+    color: 'black',
     fontSize: 20,
+    textAlign: 'center',
+    padding: 10,
+    borderRadius: 40,
+    maxWidth: 250,
+    backgroundColor: '#FFFFE0',
     fontWeight: 'bold',
-   
-  }
+    fontStyle: 'italic',
+    marginTop: 400,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 25,
+    paddingHorizontal: 10,
+    paddingVertical: 5, 
+    bottom: 450,
+    marginBottom: 150,
+  },
+  searchIcon: {
+    marginRight: 10,
+  },
+  searchInput: {
+    flex: 1,
+    fontSize: 16,
+    color: 'black',
+    paddingVertical:2, 
+    },
 });
 
 export default PlantDiseaseScreen;
